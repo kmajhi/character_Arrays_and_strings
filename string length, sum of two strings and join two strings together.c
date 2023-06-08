f2b28d19d7e08594ab7835106be5f46616086e68
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-06-08 10:47:13 +0600

    string length, sum of two strings and join two strings together.c

--- string length, sum of two strings and join two strings together.c
+++ string length, sum of two strings and join two strings together.c
@@ -2,17 +2,21 @@
 #include<string.h>
 
 int main(){
-  char str1[]= "Hello, ";
-  char str2[]= "Sujon Mahamud"; 
-  int LengthString [100];
+  char str1[] = "* * * * * Hello, ";
+  char str2[] = "Sujon Mahamud * * * * *"; 
+  char joinTwostrings[100];
   int sum;
   
   int lengthString1 = strlen(str1);
   int lengthString2 = strlen(str2);
   
-  sum = lengthString1+lengthString2;
+  sum = lengthString1 + lengthString2;
   
-  printf("Length of String 1 is = %s \nLength of String 2 is = %s\nSum of String 1 and String2 is = %d", lengthString1, lengthString2, sum);
+  strcpy(joinTwostrings, str1);
+  strcat(joinTwostrings, str2);
+
+  printf("\n\n%s\n\n", joinTwostrings);
+  printf("Length of String 1 = %d\nLength of String 2 = %d\nSum of two strings = %d\n\n", lengthString1, lengthString2, sum);
   
   return 0;
 }
